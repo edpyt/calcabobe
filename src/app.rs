82f555a602f0bada0876ec6c8b5081392476510a
edpyt <edrsa1321s@gmail.com>
2025-04-18 19:45:01 +0500

    refactor: split god Calculator component

--- src/app.rs
+++ src/app.rs
@@ -12,6 +12,8 @@ pub fn App() -> impl IntoView {
     view! { <Calculator /> }
 }
 
+type CalculatorNumber = i64;
+
 #[derive(Clone, Copy)]
 enum CalculatorOps {
     Plus,
@@ -54,98 +56,144 @@ pub fn Calculator() -> impl IntoView {
     let (state, set_state) = signal(CalculatorState::FirstNumInput);
     let (current_op, set_current_op) = signal(CalculatorOps::Plus);
 
-    let numbers = 1..=9;
-
     view! {
         <div class="h-screen grid gap-2 content-center p-12">
-            <div>
-                <span>{a}</span>
-                <input
-                    type="text"
-                    class="input input-info w-full"
-                    disabled
-                    prop:value=move || {
-                        match state.get() {
-                            CalculatorState::FirstNumInput => a,
-                            CalculatorState::SecondNumInput => b,
-                        }
-                    }
-                />
-            </div>
+            <InputNumbers a b state />
             <div class="grid grid-cols-4 gap-2">
-                <div class="col-span-3 grid grid-cols-3 content-center gap-4">
-                    {numbers
-                        .into_iter()
-                        .rev()
-                        .map(|n| {
-                            view! {
-                                <button
-                                    class="btn btn-soft"
-                                    on:click=move |_| {
-                                        let (numb, set_fn) = match state.get() {
-                                            CalculatorState::FirstNumInput => (a.get(), set_a),
-                                            CalculatorState::SecondNumInput => (b.get(), set_b),
-                                        };
-                                        let numb: i64 = format!("{}{}", numb, n)
-                                            .parse()
-                                            .expect("Can't concatenate numbers.");
-                                        set_fn.set(numb);
-                                    }
-                                >
-                                    {n}
-                                </button>
-                            }
-                        })
-                        .collect::<Vec<_>>()}
-
-                </div>
-                <div class="grid gap-2">
-                    {CalculatorOps::into_iter()
-                        .map(|op| {
-                            view! {
-                                <button
-                                    class="btn btn-soft"
-                                    on:click=move |_| {
-                                        set_current_op.set(op);
-                                        set_state.set(CalculatorState::SecondNumInput)
-                                    }
-                                >
-                                    {op.as_str()}
-                                </button>
-                            }
-                        })
-                        .collect::<Vec<_>>()}
-                </div>
+                <CalculatorButtons a set_a b set_b state />
+                <CalculatorOperationsButtons set_current_op set_state />
             </div>
             <div class="grid grid-cols-2 gap-12">
-                <button
-                    class="btn btn-soft btn-success"
-                    on:click=move |_| {
-                        let b = b.get();
-                        let mut a = set_a.write();
-                        match current_op.get() {
-                            CalculatorOps::Plus => *a += b,
-                            CalculatorOps::Mul => *a *= b,
-                            CalculatorOps::Minus => *a -= b,
-                            CalculatorOps::Div => *a /= b,
-                        };
-                        set_b.set(0);
-                        set_state.set(CalculatorState::FirstNumInput);
-                    }
-                >
-                    =
-                </button>
-
-                <button
-                    class="btn btn-soft btn-error"
-                    on:click=move |_| {
-                        *set_a.write() = 0;
-                        set_state.set(CalculatorState::FirstNumInput);
-                    }
-                >
-                    AC
-                </button>
+                <CalculatorResultButtons set_a b set_b set_state current_op />
             </div>
         </div>
+    }
+}
+
+#[component]
+fn InputNumbers(
+    a: ReadSignal<CalculatorNumber>,
+    b: ReadSignal<CalculatorNumber>,
+    state: ReadSignal<CalculatorState>,
+) -> impl IntoView {
+    view! {
+        <div>
+            <span>{a}</span>
+            <input
+                type="text"
+                class="input input-info w-full"
+                disabled
+                prop:value=move || {
+                    match state.get() {
+                        CalculatorState::FirstNumInput => a,
+                        CalculatorState::SecondNumInput => b,
+                    }
+                }
+            />
+        </div>
+    }
+}
+
+#[component]
+fn CalculatorButtons(
+    a: ReadSignal<CalculatorNumber>,
+    set_a: WriteSignal<CalculatorNumber>,
+    b: ReadSignal<CalculatorNumber>,
+    set_b: WriteSignal<CalculatorNumber>,
+    state: ReadSignal<CalculatorState>,
+) -> impl IntoView {
+    let numbers = 1..=9;
+    view! {
+        <div class="col-span-3 grid grid-cols-3 content-center gap-4">
+            {numbers
+                .into_iter()
+                .rev()
+                .map(|n| {
+                    view! {
+                        <button
+                            class="btn btn-soft"
+                            on:click=move |_| {
+                                let (numb, set_fn) = match state.get() {
+                                    CalculatorState::FirstNumInput => (a.get(), set_a),
+                                    CalculatorState::SecondNumInput => (b.get(), set_b),
+                                };
+                                let numb: CalculatorNumber = format!("{}{}", numb, n)
+                                    .parse()
+                                    .expect("Can't concatenate numbers.");
+                                set_fn.set(numb);
+                            }
+                        >
+                            {n}
+                        </button>
+                    }
+                })
+                .collect::<Vec<_>>()}
+
+        </div>
+    }
+}
+
+#[component]
+fn CalculatorOperationsButtons(
+    set_current_op: WriteSignal<CalculatorOps>,
+    set_state: WriteSignal<CalculatorState>,
+) -> impl IntoView {
+    view! {
+        <div class="grid gap-2">
+            {CalculatorOps::into_iter()
+                .map(|op| {
+                    view! {
+                        <button
+                            class="btn btn-soft"
+                            on:click=move |_| {
+                                set_current_op.set(op);
+                                set_state.set(CalculatorState::SecondNumInput)
+                            }
+                        >
+                            {op.as_str()}
+                        </button>
+                    }
+                })
+                .collect::<Vec<_>>()}
+        </div>
+    }
+}
+
+#[component]
+fn CalculatorResultButtons(
+    set_a: WriteSignal<CalculatorNumber>,
+    b: ReadSignal<CalculatorNumber>,
+    set_b: WriteSignal<CalculatorNumber>,
+    set_state: WriteSignal<CalculatorState>,
+    current_op: ReadSignal<CalculatorOps>,
+) -> impl IntoView {
+    view! {
+        <button
+            class="btn btn-soft btn-success"
+            on:click=move |_| {
+                let b = b.get();
+                let mut a = set_a.write();
+                match current_op.get() {
+                    CalculatorOps::Plus => *a += b,
+                    CalculatorOps::Mul => *a *= b,
+                    CalculatorOps::Minus => *a -= b,
+                    CalculatorOps::Div => *a /= b,
+                };
+                set_b.set(0);
+                set_state.set(CalculatorState::FirstNumInput);
+            }
+        >
+            =
+        </button>
+
+        <button
+            class="btn btn-soft btn-error"
+            on:click=move |_| {
+                *set_a.write() = 0;
+                set_state.set(CalculatorState::FirstNumInput);
+            }
+        >
+            AC
+        </button>
     }
 }
